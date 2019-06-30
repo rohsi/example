cfe2808cd8cf9177c2331458350cf15da660abf7
Rohit Sinha <rohitsinha4826@gmail.com>
2019-06-30 12:37:18 +0530

    Quote

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+
+<Quotes/>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+
+import Quotes from "./components/Quotes";
+export default {
+  name: 'App',
+  components: Quotes
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Quotes.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+
+    <h5>Text you have entered</h5>
+    <b-progress :value="value" :max="max" show-value class="mb-3"></b-progress>
+
+
+  </div>
+
+</template>
+
+
+<script>
+export default {
+  name: 'Quotes',
+  data () {
+    return {
+      value: 3,
+      max: 10
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
